078befc0c4958cb3126a2d4c55b302be69cb7ba9
012050 <beomjunchoe@naver.com>
2023-08-09 04:03:24 +0900

    멀티 스레드 참고용

---
+++ plz.py
@@ -0,0 +1,25 @@
+from multiprocessing import Process
+from DataProcess import convert_hls_to_mp4
+
+def save_multiple_streams():
+    streams = [
+        {"url": "https://klivecon-orig.fastedge.net/webrtc/test/playlist.m3u8", "output_file": "output1"},
+        {"url": "https://klivecon-orig.fastedge.net/webrtc/test/playlist.m3u8", "output_file": "output2"},
+        # 추가 스트림을 여기에 추가할 수 있습니다.
+    ]
+    
+    processes = []
+    
+    for stream in streams:
+        hls_url = stream["url"]
+        output_file = stream["output_file"]
+        
+        process = Process(target=convert_hls_to_mp4, args=(hls_url, output_file))
+        processes.append(process)
+        process.start()
+    
+    for process in processes:
+        process.join()
+
+# if __name__ == '__main__':
+#     save_multiple_streams()
