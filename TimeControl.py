eb548f772b8f99926a0e4c6a7d080b11b399b7ec
012050 <beomjunchoe@naver.com>
2023-08-10 16:57:37 +0900

    시간 관련 함수 모음

---
+++ TimeControl.py
@@ -0,0 +1,19 @@
+import time
+
+def now_time():
+    return time.strftime("%Y-%m-%d-%H:%M:%S", time.localtime(time.time()))
+
+def time_check():
+    while True:
+        current_time = time.localtime()
+        current_hour = current_time.tm_hour
+        current_minute = current_time.tm_min
+        current_second = current_time.tm_sec
+        print(f"현재 시간: {current_hour}시 {current_minute}분 {current_second}초")
+
+        # 타겟 시간에 도달하면 사용자 함수 호출
+        if current_second%20 == 0:
+            # send_data()
+            save_multiple_streams()
+
+        time.sleep(1)
