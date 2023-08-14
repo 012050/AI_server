5e22a3ab182d0d3388dd3756ac742e6bf5beaab6
012050 <beomjunchoe@naver.com>
2023-08-14 17:14:03 +0900

    데이터 전송 기본 코드

--- DataProcess.py
+++ DataProcess.py
@@ -1,5 +1,6 @@
 import os
 import ffmpeg
+import requests
 
 # URL 포맷 형식 정하기
 # URL 형태가 어떻게 되는지 확인하기
@@ -25,3 +26,23 @@ def convert_hls_to_mp4(hls_url="https://klivecon-orig.fastedge.net/webrtc/test/p
         print(f"Video saved as {output_file}")
     except ffmpeg.Error as e:
         print(f"Error occurred: {e}")
+
+# API 서버에 데이터 천송
+def send_data(url='http://localhost:5000/inteligence/activity/'):
+
+    data = {"state": "start"}
+
+    try:
+        response = requests.post(url, json=data)
+
+        if response.status_code == 200:
+            print('API 요청이 성공적으로 전송되었습니다.')
+            return 200
+        else:
+            print('API 요청 전송에 실패하였습니다.')
+            return response.status_code
+
+    except Exception as e:
+        print('API 요청 전송에 실패하였습니다.')
+        print(e)
+        return 404
