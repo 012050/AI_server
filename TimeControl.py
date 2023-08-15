c18845f9ebb0ac9ead4e827669260337efb94a1d
012050 <beomjunchoe@naver.com>
2023-08-16 01:15:18 +0900

    함수를 클래스로 변환

--- TimeControl.py
+++ TimeControl.py
@@ -5,21 +5,24 @@ from flask import request
 from DataProcess import request_data
 
 
-def now_time():
-    return time.strftime("%Y-%m-%d-%H:%M:%S", time.localtime(time.time()))
+class TimeChecker:
+    def __init__(self):
+        self.video_data = None
 
-def time_check():
-    while True:
-        current_time = time.localtime()
-        current_hour = current_time.tm_hour
-        current_minute = current_time.tm_min
-        current_second = current_time.tm_sec
-        print(f"현재 시간: {current_hour}시 {current_minute}분 {current_second}초")
+    def now_time(self):
+        return time.strftime("%Y-%m-%d-%H:%M:%S", time.localtime(time.time()))
 
-        # 타겟 시간에 도달하면 사용자 함수 호출
-        if current_second%20 == 0:
-            video_data = request_data(url='http://localhost:5000/inteligence/activity/')
-            print(video_data)
-            print("20초마다 호출되는 함수")
+    def time_check(self):
+        while True:
+            current_time = time.localtime()
+            current_hour = current_time.tm_hour
+            current_minute = current_time.tm_min
+            current_second = current_time.tm_sec
+            print(f"현재 시간: {current_hour}시 {current_minute}분 {current_second}초")
 
-        time.sleep(1)
+            # 타겟 시간에 도달하면 사용자 함수 호출
+            if current_second % 20 == 0:
+                self.video_data = request_data(url='http://localhost:5000/inteligence/activity/')
+                print("20초마다 호출되는 함수")
+
+            time.sleep(1)
