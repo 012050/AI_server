f5cdfe338260413695d3a9bc7b4ac468f0163d0c
012050 <beomjunchoe@naver.com>
2023-08-15 23:45:23 +0900

    더미 데이터

---
+++ test_server/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+video_url = "https://klivecon-orig.fastedge.net/webrtc/test/playlist.m3u8"
+
+@app.route('/inteligence/activity/', methods=['POST'])
+def count():
+    data = {"A": video_url, "B": video_url, "C": video_url, "D": video_url, "E": video_url}
+    # return jsonify({'message': 'Success'})
+    return jsonify(data)
