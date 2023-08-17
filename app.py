9b3809bffc7a8c2b398dc27792bd8e4bd4bcf3aa
012050 <beomjunchoe@naver.com>
2023-08-17 18:51:24 +0900

    다중 영상 처리

--- app.py
+++ app.py
@@ -18,3 +18,37 @@ if __name__ == '__main__':
     # 시간 확인 스레드 시작
     time_check_thread = threading.Thread(target=time_checker.time_check)
     time_check_thread.start()
+
+    while True:
+        if time_checker.video_data is not None:
+            video_data = time_checker.video_data
+            time_checker.video_data = None
+            time_checker.trans_list = []
+
+            streams = video_data
+            processes = []
+
+            # 영상 처리
+            for stream in streams:
+                hls_url = stream[1]
+                output_file = stream[0]
+
+                process = Process(target=convert_hls_to_mp4, args=(hls_url, output_file, 3))
+                processes.append(process)
+
+                # 20개로 나눠서 실행
+                if len(processes) >= 20:
+                    for process in processes:
+                        process.start()
+                    for process in processes:
+                        process.join()
+                        
+                    processes = []
+
+            # 나머지 프로세스 실행
+            for process in processes:
+                process.start()
+            for process in processes:
+                process.join()
+
+            processes = []
