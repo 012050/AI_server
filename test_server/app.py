f864c9d5543ad574c050c2c6d471595cc4aea929
012050 <beomjunchoe@naver.com>
2023-08-17 12:50:15 +0900

    Update app.py

--- test_server/app.py
+++ test_server/app.py
@@ -1,17 +1,18 @@
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-the_number = 20
+the_number = 300
 video_url = "https://klivecon-orig.fastedge.net/webrtc/test/playlist.m3u8"
 
 @app.route('/inteligence/activity/', methods=['POST'])
 def count():
     data = {}
     for i in range(1, the_number + 1):
-        if i < 10:
-            data["TEST_00"+str(i)] = video_url
-        else:
-            data["TEST_0"+str(i)] = video_url
+
+        file_name = "TEST_" + str(i).zfill(5)
+        data[file_name] = video_url
+
+
     return jsonify(data)
 
 if __name__ == '__main__':
