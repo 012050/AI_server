4c8cc8c7742b980e265ca758a6f3ca6befde0395
012050 <beomjunchoe@naver.com>
2023-08-26 12:00:25 +0900

    영상 저장 실패 3번까지 시도

--- DataProcess.py
+++ DataProcess.py
@@ -3,26 +3,36 @@ import os
 import ffmpeg
 import requests
 
+
 # 영상 저장 함수
-def convert_hls_to_mp4(hls_url="https://klivecon-orig.fastedge.net/webrtc/test/playlist.m3u8", output_file="test", duration=10):
-    file_path=os.getcwd() +  "\\videos\\" + output_file + ".mp4"
+def convert_hls_to_mp4(hls_url="https://klivecon-orig.fastedge.net/webrtc/test/playlist.m3u8", output_file="test", duration=180):
+    file_path = os.getcwd() + "\\videos\\" + output_file + ".mp4"
+    max_retry = 3
+    retry_count = 0
 
     # 파일이 존재하면 삭제
     if os.path.exists(file_path):
         try:
             os.remove(file_path)
-            print(f"File '{file_path}' deleted successfully.")
+            print(f"파일 '{file_path}' 삭제 성공")
         except Exception as e:
-            print(f"Error deleting file: {e}")
+            print(f"파일 삭제 중 오류 발생: {e}")
     else:
-        print(f"File '{file_path}' does not exist.")
+        print(f"파일 '{file_path}' 존재하지 않음")
 
-    # 영상 저장
-    try:
-        ffmpeg.input(hls_url, t=duration).output(file_path, c="copy").run()
-        print(f"Video saved as {output_file}")
-    except ffmpeg.Error as e:
-        print(f"Error occurred: {e}")
+    # 영상 저장 시도
+    while retry_count < max_retry:
+        try:
+            ffmpeg.input(hls_url, t=duration).output(file_path, c="copy").run()
+            print(f"Video saved as {output_file}")
+            break
+        except ffmpeg.Error as e:
+            print(f"오류 발생: {e}")
+            retry_count += 1
+            print(f"재시도 중... {retry_count}/{max_retry}")
+
+        if retry_count >= max_retry:
+            print("최대 재시도 횟수에 도달했습니다. 비디오를 저장하지 못했습니다.")
 
 # 인공지능 서버에 데이터 전송
 def send_data(url='http://127.0.0.1:5001/object_detection', file_name=os.getcwd() + "/videos/TEST_00001"):
