b16c916d431607a592813ec9a05ed34297ed2a14
012050 <beomjunchoe@naver.com>
2023-08-26 15:10:00 +0900

    api 수신을 위한 더미 서버

--- test_server/app.py
+++ test_server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -6,11 +6,17 @@ the_number = 23
 video_url = "https://klivecon-orig.fastedge.net/webrtc/test/playlist.m3u8"
 
 @app.route('/video/server/', methods=['POST'])
-def count():
+def video():
     data = {}
     for i in range(1, the_number + 1):
         file_name = "TEST_" + str(i).zfill(5)
         data[file_name] = video_url
+    return jsonify(data)
+
+@app.route('/inteligence/activity', methods=['POST'])
+def api_send():
+    data = request.get_json()
+    print(data)
     return jsonify(data)
 
 if __name__ == '__main__':
