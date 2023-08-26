ca4d6f2e1d18711b1d7a8aa3dc3427a90fdd9ca9
012050 <beomjunchoe@naver.com>
2023-08-27 02:22:41 +0900

    파일 이름 숫자 증가

--- test_server/app.py
+++ test_server/app.py
@@ -3,14 +3,20 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 the_number = 23
+start_number = 1
+end_number = the_number + 1
 video_url = "https://klivecon-orig.fastedge.net/webrtc/test/playlist.m3u8"
 
 @app.route('/video/server/', methods=['POST'])
 def video():
+    global start_number
+    global end_number
     data = {}
-    for i in range(1, the_number + 1):
+    for i in range(start_number, end_number):
         file_name = "TEST_" + str(i).zfill(5)
         data[file_name] = video_url
+    start_number = end_number
+    end_number = end_number + the_number
     return jsonify(data)
 
 @app.route('/inteligence/activity', methods=['POST'])
