c380da999fa1ef0264286ac0af211fe90af36708
012050 <beomjunchoe@naver.com>
2023-08-27 14:06:39 +0900

    request_video_data 함수 기본 값 변경

--- DataProcess.py
+++ DataProcess.py
@@ -57,7 +57,7 @@ def send_data(url='http://127.0.0.1:5001/object_detection', file_name=os.getcwd(
         print(e)
         return 404
 
-def request_video_data(url='http://localhost:5000/inteligence/activity/'):
+def request_video_data(url='http://localhost:5000/video/server/'):
 
     try:
         response = requests.post(url)
