2f6bd37a9b1d4de2090e5b0c7b39c4f868d4d435
012050 <beomjunchoe@naver.com>
2023-08-27 13:39:29 +0900

    랜덤 숫자 반환 함수

--- test_server/app.py
+++ test_server/app.py
@@ -1,3 +1,5 @@
+import random
+
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
@@ -6,6 +8,12 @@ the_number = 23
 start_number = 1
 end_number = the_number + 1
 video_url = "https://klivecon-orig.fastedge.net/webrtc/test/playlist.m3u8"
+
+def random_number(Number_):
+    min_value = 10 ** (Number_ - 1)
+    max_value = (10 ** Number_) - 1
+    random_number = random.randint(min_value, max_value)
+    return int(random_number)
 
 @app.route('/video/server/', methods=['POST'])
 def video():
